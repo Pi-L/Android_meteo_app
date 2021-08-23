eb39d956aa7b99c6a66bef0478408c74f6e47aca
Pi-L <47693556+Pi-L@users.noreply.github.com>
2021-08-23 12:10:26 +0200

    [TP2][WIP] 1/

--- app/src/main/java/info/legeay/meteo/MainActivity.java
+++ app/src/main/java/info/legeay/meteo/MainActivity.java
@@ -3,11 +3,7 @@ package info.legeay.meteo;
 import android.graphics.Color;
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
@@ -19,6 +15,7 @@ import info.legeay.meteo.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -27,18 +24,21 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
+    private TextView textViewCityName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_main);
         myToolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(myToolbar);
 
+        this.textViewCityName = findViewById(R.id.textview_city_name);
+        this.textViewCityName.setText(R.string.city_name);
 
-
-        Toast.makeText(this, "Loading...", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, this.textViewCityName.getText(), Toast.LENGTH_LONG).show();
     }
 
     @Override
